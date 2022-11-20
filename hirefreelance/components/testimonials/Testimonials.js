bc85a381ab2f825b5ed2e65438244d58487e6bb1
Taiye Ajimati <taiyeajimati@gmail.com>
2022-11-20 15:30:16 +0400

    Last office commit

--- hirefreelance/components/testimonials/Testimonials.js
+++ hirefreelance/components/testimonials/Testimonials.js
@@ -14,8 +14,8 @@ import 'swiper/css/scrollbar';
 
 function Testimonials() {
     return (
-        <section className="testimonials h-fit py-11 border">
-            <div className="testimonials-wrapper container flex flex-col items-center text-center border">
+        <section className="testimonials h-fit py-[140px] border flex flex-col items-center">
+            <div className="testimonials-wrapper container flex flex-col items-center text-center ">
                 <div className="title mb-12">
                     <h1 className='text-2xl text-heading'>What our Customers say</h1>
                     <p className='text-[0.8rem] max-w-[300px]'>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks.</p>
@@ -29,9 +29,14 @@ function Testimonials() {
                     <p className='text-[0.8rem] text-title lg:max-w-[380px] '>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.”</p>
                 </div> */}
 
-
             </div>
-            <Swiper className=''
+            <div className="swiper-wrap w-[96%]  lg:w-[550px] bg-white-400 h-[250px]
+            relative after:absolute after:left-[4%] after:right-[4%]  
+            after:bg-white after:z-[-1] after:top-[30%] after:bottom-[-20%] lg:after:bottom-[-6%] after:rounded-md after:shadow-lg   before:absolute before:left-[7%] before:right-[7%] 
+            before:bg-white before:z-[-1] before:top-[30%] before:bottom-[-25%] lg:before:bottom-[-12%] before:rounded-md before:shadow-lg before:opacity-70
+            ">
+          
+            <Swiper className='swiper w-[96%]  lg:w-[550px]  h-[380px] lg:h-[320px]'
                 // install Swiper modules
                 modules={[Navigation, Pagination, Scrollbar, A11y]}
                 spaceBetween={10}
@@ -39,43 +44,27 @@ function Testimonials() {
                 navigation
                 pagination={{ clickable: true }}
                 // scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
+                // onSwiper={(swiper) => console.log(swiper)}
+                // onSlideChange={() => console.log('slide change')}
             >
-                <SwiperSlide> <div className="testimonial-container flex flex-col items-center text-center w-fit border px-[50px] py-4 rounded-md">
-                    <Image className='w-16 mb-4'
-                        src={shot} />
-                    <h1 className='text-lg'>Stephan Louis</h1>
-                    <p className='text-sm mb-3 text-title'>Developer</p>
+                {testimonies.map(user=>(
+
+               
+                <SwiperSlide key={user.id}> <div className="testimonial-container bg-white flex flex-col items-center text-center w-[100%] shadow-xl  px-[50px] py-4 rounded-md mb-[40px] ">
+                    <Image className='w-16 mb-4 rounded-full'
+                        src={user.shot} 
+                        width={100} 
+                        height={100} />
+                    <h1 className='text-lg'>{user.username}</h1>
+                    <p className='text-sm mb-3 text-title'>{user.title}</p>
                     <p className='text-[0.8rem] text-title lg:max-w-[380px] '>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.”</p>
                 </div>
                 </SwiperSlide>
-                <SwiperSlide> <div className="testimonial-container flex flex-col items-center text-center w-fit border px-[50px] py-4 rounded-md">
-                    <Image className='w-16 mb-4'
-                        src={shot} />
-                    <h1 className='text-lg'>Stephan Louis</h1>
-                    <p className='text-sm mb-3 text-title'>Developer</p>
-                    <p className='text-[0.8rem] text-title lg:max-w-[380px] '>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.”</p>
-                </div>
-                </SwiperSlide>
-                <SwiperSlide> <div className="testimonial-container flex flex-col items-center text-center w-fit border px-[50px] py-4 rounded-md">
-                    <Image className='w-16 mb-4'
-                        src={shot} />
-                    <h1 className='text-lg'>Stephan Louis</h1>
-                    <p className='text-sm mb-3 text-title'>Developer</p>
-                    <p className='text-[0.8rem] text-title lg:max-w-[380px] '>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.”</p>
-                </div>
-                </SwiperSlide>
-                <SwiperSlide> <div className="testimonial-container flex flex-col items-center text-center w-fit border px-[50px] py-4 rounded-md">
-                    <Image className='w-16 mb-4'
-                        src={shot} />
-                    <h1 className='text-lg'>Stephan Louis</h1>
-                    <p className='text-sm mb-3 text-title'>Developer</p>
-                    <p className='text-[0.8rem] text-title lg:max-w-[380px] '>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.”</p>
-                </div>
-                </SwiperSlide>
-                ...
+                
+                ))}
+                
             </Swiper>
+            </div>
         </section>
     )
 }
--- hirefreelance/components/testimonials/testimonies.js
+++ hirefreelance/components/testimonials/testimonies.js
@@ -2,70 +2,56 @@
 
 const testi = [{
     "id": 1,
-    "body": "This is some awesome thinking!",
-    "shot": "https://unsplash.com/photos/Ft4p5E9HjTQ",
-    "postId": 100,
-    "user": {
-        "id": 63,
-        "username": "eburras1q"
-    }
-},
+    "body": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    "shot": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "title": 'Developer',
+     "username": "Eburras1q"
+   },
 {
     "id": 2,
     "body": "What terrific math skills you’re showing!",
-    "shot": "https://unsplash.com/photos/mpDV4xaFP8c",
-    "postId": 27,
-    "user": {
-        "id": 71,
-        "username": "omarsland1y"
-    }
+    "shot": "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "title": 'Marketing Director',
+    "username": "Omarsland1y"
+    
 },
 {
     "id": 3,
     "body": "You are an amazing writer!",
-    "shot": "https://unsplash.com/photos/pAtA8xe_iVM",
-    "postId": 61,
-    "user": {
-        "id": 29,
-        "username": "jissetts"
-    }
+    "shot": "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "title": 'HR',
+    "username": "Jissetts"
+    
 },
 {
     "id": 4,
     "body": "Wow! You have improved so much!",
-    "shot": "https://unsplash.com/photos/mEZ3PoFGs_k",
-    "postId": 8,
-    "user": {
-        "id": 19,
-        "username": "bleveragei"
-    }
-},
+    "shot": "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "title": 'Anonymous',
+    "username": "Bleveragei"
+    },
 {
     "id": 5,
     "body": "Nice idea!",
-    "shot": "https://unsplash.com/photos/Zpzf7TLj_gAk",
-    "postId": 62,
-    "user": {
-        "id": 70,
-        "username": "cmasurel1x"
-    }
+    "shot": "https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=932&q=80",
+    "title": 'student',
+     "username": "Cmasurel1x"
+    
 },
-{
-    "id": 6,
-    "body": "You are showing excellent understanding!",
-    "shot": "https://unsplash.com/photos/A73ah5JKtVA",
-    "postId": 19,
-    "user": {
-        "id": 97,
-        "username": "cdavydochkin2o"
-    }
-},
-{
-    "id": 7,
-    "body": "This is clear, concise, and complete!",
-    "shot": "https://unsplash.com/photos/p5BoBF0XJUA",
-    "postId": 47
-}
+// {
+//     "id": 6,
+//     "body": "You are showing excellent understanding!",
+//     "shot": "https://images.unsplash.com/photo-1485875437342-9b39470b3d95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+//     "title": 'Designer',
+//     "username": "cdavydochkin2o"
+//     },
+// {
+//     "id": 7,
+//     "body": "This is clear, concise, and complete!",
+//     "shot": "https://images.unsplash.com/photo-1629425733761-caae3b5f2e50?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+//     "title": 'Project Manager',
+//     "username": "Cdavydochkin2o"
+// }
 
 ]
 
